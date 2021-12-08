ceabdf56879358a05a4a3e9a1edba412737e815f
chaoyue <13832286642@163.com>
2021-12-08 17:08:45 +0800

    [ADD] 添加注释

--- senior/reptile/简书/main.go
+++ senior/reptile/简书/main.go
@@ -34,14 +34,18 @@ import (
 
 // two
 type Hot struct {
-	Href    string `selector:"div[class=content] > a[class=title]" attr:"href"`
-	Title   string `selector:"div[class=content] > a[class=title]"`
-	Summary string `selector:"div[class=content] > p[class=abstract]"`
+	Href     string `selector:"a[class=title]" attr:"href"`
+	Title    string `selector:"a[class=title]"`
+	Summary  string `selector:"p[class=abstract]"`
+	Nickname string `selector:"a[class=nickname]"`
 }
 
 func main() {
 	hots := make([]*Hot, 0)
+	// 创建 collector
 	c := colly.NewCollector(colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"), colly.MaxDepth(1), colly.Debugger(&debug.LogDebugger{}))
+
+	// 事件监听，通过 callback 执行事件处理
 	c.OnHTML("ul[class='note-list'],li", func(e *colly.HTMLElement) {
 		hot := new(Hot)
 
@@ -55,12 +59,13 @@ func main() {
 		}
 	})
 
+	// 访问 AllowedDomains 中的链接
 	err := c.Visit("https://www.jianshu.com")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
 	for _, h := range hots {
-		fmt.Println(h.Href + " || " + h.Title + " || " + h.Summary)
+		fmt.Println(h.Href + " || " + h.Title + " || " + h.Summary + " || " + h.Nickname)
 	}
 }
